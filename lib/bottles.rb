23e14f0e6bfc5cf443c83327fd4361353c4fb899
Dylan Palmboom <nofxboy1234@gmail.com>
2023-01-24 12:27:14 +1000

    pass 4 tests

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -4,7 +4,7 @@ class Bottles
   def verse(bottles)
     "#{bottles} #{bottles > 1 ? 'bottles' : 'bottle'} of beer on the wall, " \
       "#{bottles} #{bottles > 1 ? 'bottles' : 'bottle'} of beer.\n" \
-      'Take one down and pass it around, ' \
-      "#{bottles - 1} #{bottles - 1 > 1 ? 'bottles' : 'bottle'} of beer on the wall.\n"
+      "Take #{bottles > 1 ? 'one' : 'it'} down and pass it around, " \
+      "#{(bottles - 1).zero? ? 'no more' : bottles - 1} #{bottles - 1 > 1 || (bottles - 1).zero? ? 'bottles' : 'bottle'} of beer on the wall.\n"
   end
 end
