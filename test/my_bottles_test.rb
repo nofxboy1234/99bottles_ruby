04a8c6dd818c3c28c2e613adbe3563e4592ab2b2
Dylan Palmboom <nofxboy1234@gmail.com>
2023-01-27 15:54:31 +1000

    Fix spelling mistakes

--- test/my_bottles_test.rb
+++ test/my_bottles_test.rb
@@ -9,8 +9,8 @@ class BottlesTest < Minitest::Test
   def test_the_first_verse
     expected =
       '99 bottles of beer on the wall, ' \
-      "99 botlles of beer.\n" \
-      'Take one down and pass it around. ' \
+      "99 bottles of beer.\n" \
+      'Take one down and pass it around, ' \
       "98 bottles of beer on the wall.\n"
     assert_equal expected, Bottles.new.verse(99)
   end
@@ -18,8 +18,8 @@ class BottlesTest < Minitest::Test
   def test_another_verse
     expected =
       '3 bottles of beer on the wall, ' \
-      "3 botlles of beer.\n" \
-      'Take one down and pass it around. ' \
+      "3 bottles of beer.\n" \
+      'Take one down and pass it around, ' \
       "2 bottles of beer on the wall.\n"
     assert_equal expected, Bottles.new.verse(3)
   end
