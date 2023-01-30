2aead8efeb72dd8c096c54abf51dfd4891eb12f7
Dylan Palmboom <nofxboy1234@gmail.com>
2023-01-30 12:16:31 +1000

    Test verse 0 and verse 1

--- lib/my_bottles.rb
+++ lib/my_bottles.rb
@@ -3,7 +3,18 @@
 # Bottles class
 class Bottles
   def verse(number)
-    if number == 2
+    case number
+    when 0
+      'No more bottles of beer on the wall, ' \
+      "no more bottles of beer.\n" \
+      'Go to the store and buy some more, ' \
+      "99 bottles of beer on the wall.\n"
+    when 1
+      '1 bottle of beer on the wall, ' \
+      "1 bottle of beer.\n" \
+      'Take it down and pass it around, ' \
+      "no more bottles of beer on the wall.\n"
+    when 2
       '2 bottles of beer on the wall, ' \
       "2 bottles of beer.\n" \
       'Take one down and pass it around, ' \
--- test/my_bottles_test.rb
+++ test/my_bottles_test.rb
@@ -24,15 +24,6 @@ class BottlesTest < Minitest::Test
     assert_equal expected, Bottles.new.verse(3)
   end
 
-  def test_verse48
-    expected =
-      '49 bottles of beer on the wall, ' \
-      "49 bottles of beer.\n" \
-      'Take one down and pass it around, ' \
-      "48 bottles of beer on the wall.\n"
-    assert_equal expected, Bottles.new.verse(49)
-  end
-
   def test_verse2
     expected =
       '2 bottles of beer on the wall, ' \
@@ -40,5 +31,23 @@ class BottlesTest < Minitest::Test
       'Take one down and pass it around, ' \
       "1 bottle of beer on the wall.\n"
     assert_equal expected, Bottles.new.verse(2)
+  end
+
+  def test_verse_1
+    expected =
+      '1 bottle of beer on the wall, ' \
+      "1 bottle of beer.\n" \
+      'Take it down and pass it around, ' \
+      "no more bottles of beer on the wall.\n"
+    assert_equal expected, Bottles.new.verse(1)
+  end
+
+  def test_verse_0
+    expected =
+      'No more bottles of beer on the wall, ' \
+      "no more bottles of beer.\n" \
+      'Go to the store and buy some more, ' \
+      "99 bottles of beer on the wall.\n"
+    assert_equal expected, Bottles.new.verse(0)
   end
 end
