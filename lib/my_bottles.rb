83155b0f2e050ae27db39a56607b947e092fe7f6
Dylan Palmboom <nofxboy1234@gmail.com>
2023-02-05 10:53:42 +0200

    Add action method

--- lib/my_bottles.rb
+++ lib/my_bottles.rb
@@ -14,13 +14,13 @@ class Bottles
     case number
     when 0
       "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " \
-      "no more bottles of beer.\n" \
-      'Go to the store and buy some more, ' \
+      "#{quantity(number)} #{container(number)} of beer.\n" \
+      "#{action(number)}, " \
       "99 bottles of beer on the wall.\n"
     else
       "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " \
-      "#{number} #{container(number)} of beer.\n" \
-      "Take #{pronoun(number)} down and pass it around, " \
+      "#{quantity(number)} #{container(number)} of beer.\n" \
+      "#{action(number)}, " \
       "#{quantity(number - 1)} #{container(number - 1)} of beer on the wall.\n"
     end
   end
@@ -46,6 +46,14 @@ class Bottles
       'no more'
     else
       number.to_s
+    end
+  end
+
+  def action(number)
+    if number.zero?
+      'Go to the store and buy some more'
+    else
+      "Take #{pronoun(number)} down and pass it around"
     end
   end
 end
