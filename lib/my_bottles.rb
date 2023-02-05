98f7fd1a6822cd6f14bd724594c38cfb6c6dfec7
Dylan Palmboom <nofxboy1234@gmail.com>
2023-02-05 13:22:12 +0200

    Reduce #verse case statement to a single template
    verse now refers to a series of small, consistent abstractions.

--- lib/my_bottles.rb
+++ lib/my_bottles.rb
@@ -11,18 +11,10 @@ class Bottles
   end
 
   def verse(number)
-    case number
-    when 0
-      "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " \
-      "#{quantity(number)} #{container(number)} of beer.\n" \
-      "#{action(number)}, " \
-      "#{quantity(successor(number))} #{container(number - 1)} of beer on the wall.\n"
-    else
-      "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " \
-      "#{quantity(number)} #{container(number)} of beer.\n" \
-      "#{action(number)}, " \
-      "#{quantity(successor(number))} #{container(number - 1)} of beer on the wall.\n"
-    end
+    "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " \
+    "#{quantity(number)} #{container(number)} of beer.\n" \
+    "#{action(number)}, " \
+    "#{quantity(successor(number))} #{container(successor(number))} of beer on the wall.\n"
   end
 
   def container(number)
@@ -58,7 +50,7 @@ class Bottles
   end
 
   def successor(number)
-    if number == 0
+    if number.zero?
       99
     else
       number - 1
