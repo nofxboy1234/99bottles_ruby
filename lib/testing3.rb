f065619c6a1e0ee8419757750cdadaa02f6a0311
Dylan Palmboom <nofxboy1234@gmail.com>
2023-02-11 13:13:32 +0200

    Compare ruby code to c++ code in testing3.rb

--- lib/testing3.rb
+++ lib/testing3.rb
@@ -4,6 +4,7 @@ class A
   # end
 
   def a_public_method
+    p self
     a_private_method
   end
 
@@ -23,3 +24,39 @@ end
 
 puts A.new.a_public_method
 puts B.new.a_public_method
+
+# // Online C++ compiler to run C++ program
+# // https://www.programiz.com/cpp-programming/online-compiler/
+# #include <iostream>
+
+# class A {
+#     public:
+
+#     virtual int a_public_method() const { std::cout << typeid(this).name(); return a_private_method(); }
+
+#     private:
+
+#     virtual int a_private_method() const { return 777; }
+
+# };
+
+# class B : public A {
+#     public:
+
+#      int a_public_method() const override { A::a_public_method(); return 144; }
+
+#      // error: 'virtual int A::a_private_method() const' is private within this context
+#     //  void a_public_method_call_private_directly() const { a_private_method() }
+
+#     private:
+# };
+
+# int main() {
+#     // Write C++ code here
+#     A a;
+#     std::cout << a.a_public_method() << "\n";
+#     B b;
+#     std::cout << b.a_public_method() << "\n";
+
+#     return 0;
+# }
