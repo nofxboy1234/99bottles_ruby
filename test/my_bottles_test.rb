bdd921fc9e9606ae75f5902edf7d1a803885bb9a
Dylan Palmboom <nofxboy1234@gmail.com>
2023-02-15 11:15:43 +0200

    Create BottleVerseTest

--- test/my_bottles_test.rb
+++ test/my_bottles_test.rb
@@ -5,14 +5,28 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require_relative '../lib/my_bottles'
 
-class BottlesTest < Minitest::Test
+# class BottleNumberTest < Minitest::Test
+#   def test_returns_correct_class_for_given_number
+#     # 0, 1, 6 are special
+#     assert_equal BottleNumber0, BottleNumber.for(0).class
+#     assert_equal BottleNumber1, BottleNumber.for(1).class
+#     assert_equal BottleNumber6, BottleNumber.for(6).class
+
+#     # Other numbers get the default
+#     assert_equal BottleNumber, BottleNumber.for(3).class
+#     assert_equal BottleNumber, BottleNumber.for(7).class
+#     assert_equal BottleNumber, BottleNumber.for(43).class
+#   end
+# end
+
+class BottleVerseTest < Minitest::Test
   def test_the_first_verse
     expected =
       '99 bottles of beer on the wall, ' \
       "99 bottles of beer.\n" \
       'Take one down and pass it around, ' \
       "98 bottles of beer on the wall.\n"
-    assert_equal expected, Bottles.new.verse(99)
+    assert_equal expected, BottleVerse.lyrics(99)
   end
 
   def test_another_verse
@@ -21,7 +35,7 @@ class BottlesTest < Minitest::Test
       "3 bottles of beer.\n" \
       'Take one down and pass it around, ' \
       "2 bottles of beer on the wall.\n"
-    assert_equal expected, Bottles.new.verse(3)
+    assert_equal expected, BottleVerse.lyrics(3)
   end
 
   def test_verse2
@@ -30,27 +44,29 @@ class BottlesTest < Minitest::Test
       "2 bottles of beer.\n" \
       'Take one down and pass it around, ' \
       "1 bottle of beer on the wall.\n"
-    assert_equal expected, Bottles.new.verse(2)
+    assert_equal expected, BottleVerse.lyrics(2)
   end
 
-  def test_verse_1
+  def test_verse1
     expected =
       '1 bottle of beer on the wall, ' \
       "1 bottle of beer.\n" \
       'Take it down and pass it around, ' \
       "no more bottles of beer on the wall.\n"
-    assert_equal expected, Bottles.new.verse(1)
+    assert_equal expected, BottleVerse.lyrics(1)
   end
 
-  def test_verse_0
+  def test_verse0
     expected =
       'No more bottles of beer on the wall, ' \
       "no more bottles of beer.\n" \
       'Go to the store and buy some more, ' \
       "99 bottles of beer on the wall.\n"
-    assert_equal expected, Bottles.new.verse(0)
+    assert_equal expected, BottleVerse.lyrics(0)
   end
+end
 
+class BottlesTest < Minitest::Test
   def test_a_couple_verses
     expected =
       '99 bottles of beer on the wall, ' \
